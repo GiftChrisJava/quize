6eca2125c56e5b875d05b5189282c2ab9518f49f
GiftChrisJava <chrisjava77@gmail.com>
2024-02-16 09:48:01 +0200

    created some dropdowns

--- components/DropDownOptions/index.tsx
+++ components/DropDownOptions/index.tsx
@@ -10,11 +10,41 @@ import {
 export default function DropDownOptions() {
   return (
     <section className="flex justify-evenly items-center py-5">
-      <div className="px-7 py-4 w-1/3 mx-4">
+      <div className="px-7 py-4  mx-4 flex space-x-6">
         <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuTrigger className="flex outline-none text-center text-slate-800 font-bold justify-between  px-6 py-3 rounded-lg shadow-lg hover:text-slate-500 ">
+            SELECT CATEGORY
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuLabel>Select Category</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex outline-none text-center text-slate-800 font-bold justify-between  px-6 py-3 rounded-lg shadow-lg hover:text-slate-500 ">
+            SELECT LEVEL
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Level</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex outline-none text-center text-slate-800 font-bold justify-between  px-6 py-3 rounded-lg shadow-lg hover:text-slate-500 ">
+            SELECT TYPE
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Select Type</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
             <DropdownMenuItem>Billing</DropdownMenuItem>
