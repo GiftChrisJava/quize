c8ade41d49f2fd7f7360edb6065c51b79689985d
GiftChrisJava <chrisjava77@gmail.com>
2024-02-18 09:11:28 +0200

    deleted stuff

--- components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button() {
-  return (
-    <button className="py-2 px-5 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-400 dark:ring-gray-700">
-      Start Quize Now
-    </button>
-  );
-}
