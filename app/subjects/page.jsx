ba79f1440b34e0772e9c560de4b005c03dbb6420
GiftChrisJava <chrisjava77@gmail.com>
2024-02-19 16:45:25 +0200

    creatd a tutor card

--- app/subjects/page.jsx
+++ app/subjects/page.jsx
@@ -1,60 +1,126 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image from "next/image";
 import image1 from "../../public/img2.jpg";
-import image3 from "../../public/img3.jpeg";
+import cool from "../../public/cool.jpg";
 import SubjectCard from "./_components/SubjectCard";
 import { data } from "../_data";
+import FormFourSubjectCard from "./_components/FormFourSubjectCard";
 
 function Subjects() {
   return (
-    <div className="max-container ">
-      <article className="flex flex-row sm:gap-8 items-center justify-center">
-        <section className="flex flex-2 sm:flex-1 flex-col p-4">
-          <h2 className="font-extrabold flex-start sm:text-2xl md:text-4xl text-gray-900 ">
-            <span className="text-green-600">MALAWI</span> EDUCATION ENHANCEMENT
-            <span className="text-red-600"> PLATFORM</span>
-          </h2>
-          <h4 className=" font-bold text-sm text-gray-600 mt-4">
-            A solution to help you understand the most challenging and complex
-            topics in MSCE level classes and find tutors for further help
+    <container className=" ">
+      <section className="bg-white">
+        <article className="max-container pb-10 flex flex-row sm:gap-8 items-center justify-center">
+          <section className="flex flex-2 sm:flex-1 flex-col p-4">
+            <h2 className="font-extrabold flex-start sm:text-2xl md:text-4xl text-gray-900 ">
+              <span className="text-green-600">MALAWI</span> EDUCATION
+              ENHANCEMENT
+              <span className="text-red-600"> PLATFORM</span>
+            </h2>
+            <h4 className=" font-bold text-sm text-gray-600 mt-4">
+              A solution to help you understand the most challenging and complex
+              topics in MSCE level classes and find tutors for further help
+            </h4>
+
+            <h2 className=" font-bold text-gray-800 py-4 mt-5 sm:text-md text-sm">
+              Select a subject below begin your learning based on your current
+              class
+            </h2>
+          </section>
+          <section className="flex-1 ml-10">
+            <Image
+              src={image1}
+              width={280}
+              height={235}
+              alt="student"
+              className="rounded-lg hidden object-contain sm:flex"
+            />
+          </section>
+        </article>
+      </section>
+
+      <section className="max-container">
+        <article className="sm:4 p-1 mt-8">
+          <h4 className="text-center md:text-left font-bold text-lg mt-4 text-red-600">
+            Form 4 Subjects
           </h4>
+          {/* form 4 subjects  */}
 
-          <h2 className=" font-bold text-gray-800 py-4 mt-5 sm:text-md text-sm">
-            Select a subject below begin your learning based on your current
-            class
-          </h2>
-        </section>
-        <section className="flex-1 ml-10">
-          <Image
-            src={image1}
-            width={280}
-            height={235}
-            alt="student"
-            className="rounded-lg hidden object-contain sm:flex"
-          />
-        </section>
-      </article>
+          <div className="grid md:grid-cols-4 gap-1 sm:grid-cols-3 sm:gap-4 grid-cols-1">
+            {data.map((subject) => (
+              <div key={subject.id}>
+                <FormFourSubjectCard
+                  className="flex"
+                  name={subject.name}
+                  image={subject.image}
+                  id={subject.id}
+                />
+              </div>
+            ))}
+          </div>
+        </article>
 
-      <article className="sm:4 p-1">
-        <h4 className="text-center md:text-left font-bold text-lg mt-4 text-green-700">
-          Form 3 Subjects
-        </h4>
-        {/* form 3 subjects  */}
+        <article className="sm:4 p-1 mt-10">
+          <h4 className="text-center md:text-left font-bold text-lg mt-4 text-green-700">
+            Form 3 Subjects
+          </h4>
+          {/* form 3 subjects  */}
 
-        <div className="grid md:grid-cols-4 gap-1 sm:grid-cols-3 sm:gap-4 grid-cols-1">
-          {data.map((subject) => (
-            <div key={subject.id}>
-              <SubjectCard
-                className="flex"
-                name={subject.name}
-                image={subject.image}
-                id={subject.id}
-              />
+          <div className="grid md:grid-cols-4 gap-1 sm:grid-cols-3 sm:gap-4 grid-cols-1">
+            {data.map((subject) => (
+              <div key={subject.id}>
+                <SubjectCard
+                  className="flex"
+                  name={subject.name}
+                  image={subject.image}
+                  id={subject.id}
+                />
+              </div>
+            ))}
+          </div>
+        </article>
+      </section>
+
+      <section className="bg-green-600 mt-10 pb-5">
+        <article className="max-container">
+          <h4 className="font-bold text-2xl text-white">Find a Tutor</h4>
+          <small className="font-bold text-gray-900">
+            Here is a list of tutors to you can hire for further help
+          </small>
+        </article>
+
+        <article className="py-3 px-1 shadow-xl bg-white rounded-md mx-auto max-w-[220px] md:max-w-[220px]">
+          <div className="flex flex-col items-center justify-center">
+            <Image
+              src={cool}
+              height={100}
+              width={100}
+              alt="tutor"
+              className="rounded-full object-contain h-34 w-30"
+            />
+          </div>
+
+          <div>
+            <h3 className="text-center mt-2 font-bold text-gray-900 text-lg">
+              Patrick Chemusa
+            </h3>
+            <p className="text-center text-sm text-gray-700">
+              Am a teacher with specialization in{" "}
+              <span className="font-bold">Maths</span> and{" "}
+              <span className="font-bold"> Physis</span>
+            </p>
+
+            <p className="text-center mt-2 text-gray-700 text-sm">
+              Based in Lilongwe
+            </p>
+
+            <div className="flex flex-col mt-3 items-center justify-center">
+              <small className="text-red-600 font-bold">Find out more</small>
             </div>
-          ))}
-        </div>
-      </article>
-    </div>
+          </div>
+        </article>
+      </section>
+    </container>
   );
 }
 
