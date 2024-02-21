7dca4f02cba7f577e7d551c97bb1b78a1815b3a1
GiftChrisJava <chrisjava77@gmail.com>
2024-02-21 17:30:08 +0200

    topics data

---
+++ app/subjects/[subject_id]/_component/topics.js
@@ -0,0 +1,104 @@
+export const topics = [
+  {
+    id: 1,
+    name: "Using Spreadsheets",
+    subtopics: [
+      {
+        id: 1,
+        name: "working with a spreadsheet",
+        description: "Data entry, editing, formating and printing a worksheet",
+      },
+      {
+        id: 2,
+        name: "Formulae and functions",
+        description: "",
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Desktop publishing software",
+    subtopics: [
+      {
+        id: 1,
+        name: "working desktop publishing software",
+        description: "Creating, formating, editing, saving, printing, etc",
+      },
+      {
+        id: 2,
+        name: "Designing publications",
+        description: "Types, orientation, layout, margins, text boxes, etc",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "Software installation",
+    subtopics: [
+      {
+        id: 1,
+        name: "Operating system (OS)",
+        description: "Have a good understand of the OS",
+      },
+      {
+        id: 2,
+        name: "Troubleshoot computers",
+        description: "Understand computer hardware and its software",
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "Communication technologies",
+    subtopics: [
+      {
+        id: 1,
+        name: "Uses of networks",
+        description:
+          "Learn how communication networks are used in everyday life",
+      },
+      {
+        id: 2,
+        name: "Communication systems network devices",
+        description:
+          "Modems, routers, transmitters, receivers, switches, repeaters, etc",
+      },
+      {
+        id: 3,
+        name: "Transmission techniques",
+        description: "Understand the complex techniques of transmission",
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    name: "Programming fundamentals",
+    subtopics: [
+      {
+        id: 1,
+        name: "Definitions",
+        description:
+          "Have a good understanding of compilers, translators, assembler",
+      },
+      {
+        id: 2,
+        name: "Programming basics",
+        description: "Structure, variables, compiling and running",
+      },
+      {
+        id: 3,
+        name: "Operators",
+        description: "Understand arithmetic and logical operators",
+      },
+      {
+        id: 4,
+        name: "Control structures",
+        description: "Sequence, selection, loops",
+      },
+    ],
+  },
+];
