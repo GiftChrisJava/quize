c5a09afc06ce53554c9147d9e7d0597003b6b6b4
GiftChrisJava <chrisjava77@gmail.com>
2024-04-08 11:45:40 +0200

    added literacy topics

---
+++ app/subjects/[subject_id]/ninja/literacyTopics.js
@@ -0,0 +1,122 @@
+export const computerLiteracyTopics = [
+  {
+    id: 1,
+    topic_title: "How to Create a Google Account",
+    description:
+      "Learn how to set up your own Google account to access various Google services.",
+    video_url: "https://youtu.be/SUecegkK5GM?si=lmlaWK0PxO38tlC6",
+  },
+  {
+    id: 2,
+    topic_title: "How to Install Windows 10",
+    description:
+      "Step-by-step guide on installing the Windows 10 operating system on your computer.",
+    video_url: "https://youtu.be/nbGkPYtXtmA?si=D6n08dHmIyu-eoR3",
+  },
+  {
+    id: 3,
+    topic_title: "How to Write an Email",
+    description:
+      "Master the art of composing emails for effective communication.",
+    video_url: "https://youtu.be/fSHQ-oi3pDc?si=qKyYfLggW8OREFGK",
+  },
+  {
+    id: 4,
+    topic_title: "How to Use Google Calendar",
+    description:
+      "Organize your schedule efficiently using Google Calendar's features.",
+    video_url: "https://youtu.be/6dyCOXNLp8U?si=vOGZoxsjZpQhHMJ7",
+  },
+  {
+    id: 5,
+    topic_title: "How to Use Google Search Engine",
+    description:
+      "Unlock the full potential of Google's search engine to find information quickly and effectively.",
+    video_url: "https://youtu.be/NYz7m88stJA?si=TtsY7JYibwhUrQjb",
+  },
+  {
+    id: 6,
+    topic_title: "Introduction to Google Drive",
+    description:
+      "Learn about Google Drive and how to store, access, and share files online.",
+    video_url: "https://youtu.be/EbVnObwFJic?si=nUOvMwhP1vvxPVII",
+  },
+  {
+    id: 7,
+    topic_title: "How to Connect to WiFi",
+    description:
+      "Connect your devices to wireless networks and stay connected wherever you go.",
+    video_url: "https://youtu.be/vjnHrsupqx4?si=F81pUDCbONVdH45M",
+  },
+  {
+    id: 8,
+    topic_title: "How to Start a Conversation",
+    description:
+      "Develop conversational skills for effective communication in various social settings.",
+    video_url: "https://youtu.be/Z4_j1wPy-Ng?si=F9s51q7F7Cuv4InQ",
+  },
+  {
+    id: 9,
+    topic_title: "Cybersecurity Basics",
+    description:
+      "Understand essential concepts and practices to protect yourself from online threats.",
+    video_url: "https://youtu.be/aRbKFCY4tjE?si=b6l2i8XA8i0XW3Vl",
+  },
+  {
+    id: 10,
+    topic_title: "Budgeting Money",
+    description:
+      "Learn strategies for managing and allocating your finances effectively.",
+    video_url: "https://youtu.be/CbhjhWleKGE?si=D_bTIryp_Go9XO8Q",
+  },
+  {
+    id: 11,
+    topic_title:
+      "Digital Etiquette: Online Manners and Responsible Social Media Use",
+    description:
+      "Navigate the digital world with proper etiquette and responsible behavior on social media platforms.",
+    video_url: "https://youtu.be/ottnH427Fr8?si=92S_j1Th92J_r_7t",
+  },
+  {
+    id: 12,
+    topic_title: "How to Take Good Photos Using a Phone",
+    description:
+      "Capture stunning photos using your smartphone's camera features and techniques.",
+    video_url: "https://youtu.be/kVw12Uhk1sI?si=RnaoQvCWK0boenmp",
+  },
+  {
+    id: 13,
+    topic_title: "How to Use an ATM Machine",
+    description:
+      "Learn the basics of operating an ATM machine for easy access to banking services.",
+    video_url: "https://youtu.be/GAt3AgWqwV4?si=rpP0vStKk_yWiIV2",
+  },
+  {
+    id: 14,
+    topic_title: "How to Use YouTube",
+    description:
+      "Discover how to navigate YouTube's interface and explore its vast library of videos.",
+    video_url: "https://youtu.be/LicfHxvAlXI?si=lqBqr7KE_1rHoXF1",
+  },
+  {
+    id: 15,
+    topic_title: "Sending and Receiving Files Using Bluetooth",
+    description:
+      "Transfer files wirelessly between devices using Bluetooth technology.",
+    video_url: "https://youtu.be/8dpssAzWKwQ?si=f_dQAbAClKQGh9uk",
+  },
+  {
+    id: 16,
+    topic_title: "How to Use a Pocket MiFi",
+    description:
+      "Get the most out of your pocket MiFi device for on-the-go internet access.",
+    video_url: "https://youtu.be/Eg07mAfOWtg?si=2_cEWoMDx_Nvgeo1",
+  },
+  {
+    id: 17,
+    topic_title: "Intro to Canva",
+    description:
+      "Discover how create really cool designs and presentation slides",
+    video_url: "https://youtu.be/BrUrsnO8OYg?si=o8Ls060OQjMPXzUe",
+  },
+];
--- app/subjects/[subject_id]/ninja/page.jsx
+++ app/subjects/[subject_id]/ninja/page.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Ninja() {
-  return <div>Ninja</div>;
+  return (
+    <div className="max-container max-w-3xl mx-auto p-3">
+      <div>
+        <h2 className="text-lg text-gray-800 font-bold">
+          Outside The Classroom
+        </h2>
+        <p className="mt-8">
+          Hey, Hey, Hey! 🎉 Big shoutout to you for making the smart move to
+          check out this section! 🚀 Get ready for a fun ride because
+          we&lsquo;ve got some seriously cool videos lined up to supercharge
+          your computer skills and make you a tech whiz! 💻 Say goodbye to
+          boring lectures because here, learning feels like a breeze! 🌬️ So
+          buckle up and get ready to dive into the world of computer literacy
+          with a big smile! 😄 Let&lsquo;s make learning a blast together! 🌟
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Ninja;
