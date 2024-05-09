01fde67d7f327a4969aa56d886eb4fee2544c907
GiftChrisJava <chrisjava77@gmail.com>
2024-05-09 22:38:14 +0200

    added ninja images

--- app/subjects/[subject_id]/ninja/literacyTopics.js
+++ app/subjects/[subject_id]/ninja/literacyTopics.js
@@ -1,7 +1,22 @@
-import image3 from "../../../../public/img3.jpeg";
-import image5 from "../../../../public/img5.jpeg";
-import image6 from "../../../../public/img6.jpeg";
-import image7 from "../../../../public/img7.jpeg";
+import google from "../../../../public/google.jpg";
+import windows from "../../../../public/windows.jpg";
+import gmail from "../../../../public/gmail.jpg";
+import calender from "../../../../public/calender.jpg";
+import engine from "../../../../public/engine.jpg";
+import drive from "../../../../public/drive.jpg";
+import wifi from "../../../../public/wifi.jpg";
+import conva from "../../../../public/conva.jpg";
+import cyber from "../../../../public/cyber.jpg";
+import budget from "../../../../public/budget.jpg";
+import digital from "../../../../public/digital.jpg";
+import photo from "../../../../public/photo.jpg";
+import atm from "../../../../public/atm.jpg";
+import youtube from "../../../../public/youtube.jpg";
+import blue from "../../../../public/blue.jpg";
+import mifi from "../../../../public/mifi.jpg";
+import canva from "../../../../public/canva.jpg";
+
+
 
 export const computerLiteracyTopics = [
   {
@@ -10,7 +25,7 @@ export const computerLiteracyTopics = [
     description:
       "Learn how to set up your own Google account to access various Google services.",
     video_url: "https://youtu.be/SUecegkK5GM?si=lmlaWK0PxO38tlC6",
-    thumbnail: image3,
+    thumbnail: google,
   },
   {
     id: 2,
@@ -18,7 +33,7 @@ export const computerLiteracyTopics = [
     description:
       "Step-by-step guide on installing the Windows 10 operating system on your computer.",
     video_url: "https://youtu.be/nbGkPYtXtmA?si=D6n08dHmIyu-eoR3",
-    thumbnail: image5,
+    thumbnail: windows,
   },
   {
     id: 3,
@@ -26,7 +41,7 @@ export const computerLiteracyTopics = [
     description:
       "Master the art of composing emails for effective communication.",
     video_url: "https://youtu.be/fSHQ-oi3pDc?si=qKyYfLggW8OREFGK",
-    thumbnail: image6,
+    thumbnail: gmail,
   },
   {
     id: 4,
@@ -34,7 +49,7 @@ export const computerLiteracyTopics = [
     description:
       "Organize your schedule efficiently using Google Calendar's features.",
     video_url: "https://youtu.be/6dyCOXNLp8U?si=vOGZoxsjZpQhHMJ7",
-    thumbnail: image7,
+    thumbnail: calender,
   },
   {
     id: 5,
@@ -42,7 +57,7 @@ export const computerLiteracyTopics = [
     description:
       "Unlock the full potential of Google's search engine to find information quickly and effectively.",
     video_url: "https://youtu.be/NYz7m88stJA?si=TtsY7JYibwhUrQjb",
-    thumbnail: image3,
+    thumbnail: engine,
   },
   {
     id: 6,
@@ -50,7 +65,7 @@ export const computerLiteracyTopics = [
     description:
       "Learn about Google Drive and how to store, access, and share files online.",
     video_url: "https://youtu.be/EbVnObwFJic?si=nUOvMwhP1vvxPVII",
-    thumbnail: image5,
+    thumbnail: drive,
   },
   {
     id: 7,
@@ -58,7 +73,7 @@ export const computerLiteracyTopics = [
     description:
       "Connect your devices to wireless networks and stay connected wherever you go.",
     video_url: "https://youtu.be/vjnHrsupqx4?si=F81pUDCbONVdH45M",
-    thumbnail: image6,
+    thumbnail: wifi,
   },
   {
     id: 8,
@@ -66,7 +81,7 @@ export const computerLiteracyTopics = [
     description:
       "Develop conversational skills for effective communication in various social settings.",
     video_url: "https://youtu.be/Z4_j1wPy-Ng?si=F9s51q7F7Cuv4InQ",
-    thumbnail: image7,
+    thumbnail: conva,
   },
   {
     id: 9,
@@ -74,7 +89,7 @@ export const computerLiteracyTopics = [
     description:
       "Understand essential concepts and practices to protect yourself from online threats.",
     video_url: "https://youtu.be/aRbKFCY4tjE?si=b6l2i8XA8i0XW3Vl",
-    thumbnail: image3,
+    thumbnail: cyber,
   },
   {
     id: 10,
@@ -82,7 +97,7 @@ export const computerLiteracyTopics = [
     description:
       "Learn strategies for managing and allocating your finances effectively.",
     video_url: "https://youtu.be/CbhjhWleKGE?si=D_bTIryp_Go9XO8Q",
-    thumbnail: image5,
+    thumbnail: budget,
   },
   {
     id: 11,
@@ -91,7 +106,7 @@ export const computerLiteracyTopics = [
     description:
       "Navigate the digital world with proper etiquette and responsible behavior on social media platforms.",
     video_url: "https://youtu.be/ottnH427Fr8?si=92S_j1Th92J_r_7t",
-    thumbnail: image6,
+    thumbnail: digital,
   },
   {
     id: 12,
@@ -99,7 +114,7 @@ export const computerLiteracyTopics = [
     description:
       "Capture stunning photos using your smartphone's camera features and techniques.",
     video_url: "https://youtu.be/kVw12Uhk1sI?si=RnaoQvCWK0boenmp",
-    thumbnail: image7,
+    thumbnail: photo,
   },
   {
     id: 13,
@@ -107,7 +122,7 @@ export const computerLiteracyTopics = [
     description:
       "Learn the basics of operating an ATM machine for easy access to banking services.",
     video_url: "https://youtu.be/GAt3AgWqwV4?si=rpP0vStKk_yWiIV2",
-    thumbnail: image3,
+    thumbnail: atm,
   },
   {
     id: 14,
@@ -115,7 +130,7 @@ export const computerLiteracyTopics = [
     description:
       "Discover how to navigate YouTube's interface and explore its vast library of videos.",
     video_url: "https://youtu.be/LicfHxvAlXI?si=lqBqr7KE_1rHoXF1",
-    thumbnail: image5,
+    thumbnail: youtube,
   },
   {
     id: 15,
@@ -123,7 +138,7 @@ export const computerLiteracyTopics = [
     description:
       "Transfer files wirelessly between devices using Bluetooth technology.",
     video_url: "https://youtu.be/8dpssAzWKwQ?si=f_dQAbAClKQGh9uk",
-    thumbnail: image6,
+    thumbnail: blue,
   },
   {
     id: 16,
@@ -131,14 +146,14 @@ export const computerLiteracyTopics = [
     description:
       "Get the most out of your pocket MiFi device for on-the-go internet access.",
     video_url: "https://youtu.be/Eg07mAfOWtg?si=2_cEWoMDx_Nvgeo1",
-    thumbnail: image7,
+    thumbnail: mifi,
   },
   {
     id: 17,
-    topic_title: "Intro to Canva",
+    topic_title: "Intro to Designing Using Canva",
     description:
       "Discover how create really cool designs and presentation slides",
     video_url: "https://youtu.be/BrUrsnO8OYg?si=o8Ls060OQjMPXzUe",
-    thumbnail: image5,
+    thumbnail: canva,
   },
 ];
