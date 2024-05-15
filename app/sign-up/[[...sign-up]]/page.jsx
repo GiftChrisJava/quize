e9f6ebe506d5926634be27dbf9588e42b54660d0
GiftChrisJava <chrisjava77@gmail.com>
2024-05-15 22:57:02 +0200

    oky

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
-        <SignUp path="/sign-up" className="ml-6"/>;
+        <SignUp path="/sign-up" className="ml-6"/>
     </div>
   )
 }
