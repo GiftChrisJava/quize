d98326f4128a7250efaef3f57aa54c35f1fbcc99
GiftChrisJava <chrisjava77@gmail.com>
2024-06-11 17:02:39 +0200

    done with quize component

--- app/quizes/_components/QuizComponent.jsx
+++ app/quizes/_components/QuizComponent.jsx
@@ -1,14 +1,15 @@
 "use client";
 import { useState } from "react";
-import { questions } from "../constants/questions";
 import award from "../../../public/award.gif";
 import trophy from "../../../public/trophy.gif";
 import origami from "../../../public/origami.gif";
 import sad from "../../../public/sad.gif";
-
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-const QuizComponent = ({ path }) => {
+
+const QuizComponent = ({ path, quizData }) => {
+  const questions = quizData.questions;
+
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [attempts, setAttempts] = useState(0);
@@ -19,18 +20,14 @@ const QuizComponent = ({ path }) => {
   const [flag, setFlag] = useState(true);
   const [halfwayAcknowledged, setHalfwayAcknowledged] = useState(false);
 
-  // Function to handle option selection for multiple correct answers
   const handleOptionSelect = (option) => {
-    // Check if the current question allows multiple answers
     if (questions[currentQuestion].allowsMultiple) {
-      // If it does, toggle the option in the selectedOptions array
       if (selectedOptions.includes(option)) {
         setSelectedOptions(selectedOptions.filter((opt) => opt !== option));
       } else {
         setSelectedOptions([...selectedOptions, option]);
       }
     } else {
-      // If only one answer is allowed, set the selected option directly
       setSelectedOptions([option]);
     }
     setIsOptionSelected(true);
@@ -39,13 +36,11 @@ const QuizComponent = ({ path }) => {
   const router = useRouter();
 
   const onFinish = () => {
-    // Redirect the user to the desired URL
     router.push(`${path}`);
   };
 
-  // Function to handle answer submission for multiple correct answers
   const handleAnswerSubmit = () => {
-    const correctAnswers = questions[currentQuestion].answers;
+    const correctAnswers = [questions[currentQuestion].correctAnswer];
     const isAnswerCorrect =
       selectedOptions.sort().join(",") === correctAnswers.sort().join(",");
     setIsCorrect(isAnswerCorrect);
@@ -55,30 +50,29 @@ const QuizComponent = ({ path }) => {
         setScore(score + 1);
         setCurrentQuestion(currentQuestion + 1);
         setAttempts(0);
-        setSelectedOptions([]); // Reset for the next question
-        setIsCorrect(false); // Reset for the next question
-      }, 1000); // Delay to show the green tick before moving to the next question
+        setSelectedOptions([]);
+        setIsCorrect(false);
+      }, 1000);
     } else {
       setAttempts(attempts + 1);
       if (attempts >= 1) {
         setTimeout(() => {
           setCurrentQuestion(currentQuestion + 1);
           setAttempts(0);
-          setSelectedOptions([]); // Reset for the next question
-          setIsCorrect(false); // Reset for the next question
-        }, 1000); // Delay to show the red cross before moving to the next question
+          setSelectedOptions([]);
+          setIsCorrect(false);
+        }, 1000);
       }
     }
 
     setIsOptionSelected(false);
   };
 
-  // Function to render the progress bar
   const renderProgressBar = () => {
     const progress = ((currentQuestion + 1) / questions.length) * 100;
     return (
       <div
-        className="progress-bar-container "
+        className="progress-bar-container"
         style={{ width: "100%", backgroundColor: "#e0e0e0" }}
       >
         <div
@@ -98,7 +92,6 @@ const QuizComponent = ({ path }) => {
     setShowResult(true);
   };
 
-  // Function to handle the "Retry" button
   const handleRetry = () => {
     setCurrentQuestion(0);
     setScore(0);
@@ -108,9 +101,8 @@ const QuizComponent = ({ path }) => {
     setShowResult(false);
   };
 
-  // Function to render the result screen
   const renderResult = () => {
-    const percentage = (score / questions.length) * 100;
+    const percentage = Math.round((score / questions.length) * 100);
     return (
       <div className="bg-white shadow-xl p-4 rounded-md">
         {percentage > 79 ? (
@@ -122,7 +114,6 @@ const QuizComponent = ({ path }) => {
               width={200}
               className="flex flex-col justify-center items-center max-w-sm mx-auto"
             />
-
             <p className="text-center font-bold text-green-700">
               Excellent!! You Killed it
             </p>
@@ -136,7 +127,6 @@ const QuizComponent = ({ path }) => {
               width={200}
               className="flex flex-col justify-center items-center max-w-sm mx-auto"
             />
-
             <p className="text-center font-bold text-green-700">
               Congratulations!
             </p>
@@ -150,7 +140,6 @@ const QuizComponent = ({ path }) => {
               width={200}
               className="flex flex-col justify-center items-center max-w-sm mx-auto"
             />
-
             <p className="text-center font-bold text-green-700">
               Good work! Keep On Practicing for Improvement
             </p>
@@ -164,18 +153,15 @@ const QuizComponent = ({ path }) => {
               width={200}
               className="flex flex-col justify-center items-center max-w-sm mx-auto"
             />
-
             <p className="text-center font-bold text-green-700">
-              Oops!! That was not so great. Do not loose heart, Keep on
+              Oops!! That was not so great. Do not lose heart, Keep on
               Practicing
             </p>
           </div>
         )}
-
         <p className="text-center font-bold text-green-700 text-xl">
           {percentage}%
         </p>
-
         <p className="text-center font-bold text-gray-700 mt-2 mb-5">
           {score}/{questions.length}
         </p>
@@ -185,13 +171,13 @@ const QuizComponent = ({ path }) => {
         >
           <button
             className="p-2 rounded mt-3 border border-slate-600 bg-gray-700 text-gray-200"
-            onClick={() => handleRetry()}
+            onClick={handleRetry}
           >
             Retry Quiz
           </button>
           <button
             className="p-2 rounded mt-3 border border-slate-600 bg-gray-700 text-gray-200"
-            onClick={() => onFinish()}
+            onClick={onFinish}
           >
             Exit Quiz
           </button>
@@ -200,16 +186,12 @@ const QuizComponent = ({ path }) => {
     );
   };
 
-  // Check if the user has reached half of the questions and if the halfway point has been acknowledged
   const isHalfwayThrough =
     currentQuestion === Math.ceil(questions.length / 2) && !halfwayAcknowledged;
 
-  // Function to render the encouraging message and continue button
   const renderEncouragement = () => (
     <div data-aos="fade-top" className="encouragement-message text-center p-4">
-      <h2 className="text-2xl font-semibold text-green-600">
-        Great job so far!
-      </h2>
+      <h2 className="text-2xl font-semibold text-green-600">Great job so far!</h2>
       <p className="text-md text-gray-100">
         Take a deep breath and keep up the good work.
       </p>
@@ -225,10 +207,7 @@ const QuizComponent = ({ path }) => {
   return (
     <main className="flex flex-col justify-center items-center mt-10 md:w-[700px] md:mx-auto">
       <div className="quiz-container sm:max-container max-w-[400px] bg-gray-700 rounded">
-        {/* Progress bar */}
         {flag && renderProgressBar()}
-
-        {/* Feedback based on attempts */}
         {attempts > 0 && (
           <div className="feedback-container mt-4">
             {attempts === 1 && (
@@ -241,11 +220,7 @@ const QuizComponent = ({ path }) => {
             )}
           </div>
         )}
-
-        {/* Render encouragement message if halfway through and not acknowledged */}
         {isHalfwayThrough && renderEncouragement()}
-
-        {/* Render the next question only if halfwayAcknowledged is true */}
         {!isHalfwayThrough &&
           !showResult &&
           currentQuestion < questions.length && (
@@ -253,14 +228,12 @@ const QuizComponent = ({ path }) => {
               <div className="flex gap-2 md:justify-center">
                 <p
                   data-aos="zoom-in"
-                  className="text-md md:text-xl font-semibold text-gray-300 "
+                  className="text-md md:text-xl font-semibold text-gray-300"
                 >
-                  {questions[currentQuestion].number}
-                  {"."}
+                  {currentQuestion + 1}.
                 </p>
-
-                <h2 className="text-md md:text-2xl font-normal text-gray-300 ">
-                  {questions[currentQuestion].question}
+                <h2 className="text-md md:text-2xl font-normal text-gray-300">
+                  {questions[currentQuestion].questionText}
                 </h2>
               </div>
               <div className="md:mt-10 mt-5 sm:grid sm:grid-flow-col sm:gap-1">
@@ -280,7 +253,9 @@ const QuizComponent = ({ path }) => {
                     </div>
                     {selectedOptions.includes(option) &&
                       questions[currentQuestion].correctAnswer === option && (
-                        <span className="text-green-500 ml-2">✓</span>
+                        <span className="text-green-500 ml-2"></span>
+                        // <span className="text-green-500 ml-2">✓</span>
+
                       )}
                   </div>
                 ))}
@@ -298,19 +273,16 @@ const QuizComponent = ({ path }) => {
               </button>
             </div>
           )}
-
-        {/* show result screen at the end  */}
         <div className="bg-[#f0f0f0f0]">
           {currentQuestion === questions.length && !showResult && (
             <button
               className="mt-10 ml-8 text-red-600 hover:text-gray-800"
-              onClick={() => removeProgressBar()}
+              onClick={removeProgressBar}
             >
               Click Me To See Results
             </button>
           )}
         </div>
-
         {showResult && renderResult()}
       </div>
     </main>
