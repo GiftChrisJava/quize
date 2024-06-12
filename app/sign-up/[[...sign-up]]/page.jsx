a4271ceb62c5c530d14ebd3e546d46cf09f960fc
GiftChrisJava <chrisjava77@gmail.com>
2024-06-12 21:56:08 +0200

    deleted

--- app/sign-up/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-        <SignUp path="/sign-up" className="ml-6"/>
-    </div>
-  )
-}
