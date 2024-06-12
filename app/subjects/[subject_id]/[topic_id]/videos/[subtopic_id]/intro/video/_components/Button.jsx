e7cc5988c052be176c6559397a0e79bc58274982
GiftChrisJava <chrisjava77@gmail.com>
2024-06-12 21:22:33 +0200

    ok

--- app/subjects/[subject_id]/[topic_id]/videos/[subtopic_id]/intro/video/_components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ name }) {
-  return (
-    <button className="mt-1 px-3 py-2 font-semibold text-sm bg-slate-600 hover:text-green-600 rounded-md text-gray-200 w-38">
-      {name}
-    </button>
-  );
-}
-
-export default Button;
