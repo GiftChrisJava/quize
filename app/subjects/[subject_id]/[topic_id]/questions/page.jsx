000ee0abd75dbc40b3607a6b6cf2ab33a47dd968
bed-com-25-19 <bed-com-25-19@unima.ac.mw>
2024-06-14 06:35:43 +0200

    not yet uploaded the test

--- app/subjects/[subject_id]/[topic_id]/questions/page.jsx
+++ app/subjects/[subject_id]/[topic_id]/questions/page.jsx
@@ -108,6 +108,19 @@ function Questions({ params }) {
                 
               </button>
             </article>
+
+            {(testQuestions.length > 0) ? (
+                 <article className="flex flex-row justify-center items-center mt-5 mb-5">
+                 <button
+                     className="mt-1 rounded px-3 py-2 font-semibold text-sm bg-slate-600 hover:text-green-600 text-gray-200 w-38"
+                 >
+                   <Link href={`/subjects/${subject_id}/${topic_id}/questions/results`} >View Test Results</Link>
+                   
+                 </button>
+               </article>
+            ) : (
+                <div className="font-bold justify-center max-md mx-auto">Not Yet Uploaded</div>
+            )}
             {!submitted && (
                 <h5 className="text-center text-sm text-gray-800 mb-8 mt-8">
                     Click on the question to write an answer.{" "}
