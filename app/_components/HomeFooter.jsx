03b0dd2e959fd0d29dcc4b4f5928e3898f746011
bed-com-25-19 <bed-com-25-19@unima.ac.mw>
2024-06-14 08:40:31 +0200

    mt

--- app/_components/HomeFooter.jsx
+++ app/_components/HomeFooter.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function HomeFooter() {
 
   return (
-    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 mt-14 md:mt-4">
     <hr className='mb-10'/>
   <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
     <div className="sm:col-span-2">
